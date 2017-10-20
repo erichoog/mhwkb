862a2cf78fbcda9fc47964a21887dfb92ed9193b
Eric Hoog <eric_hoog@hotmail.com>
2017-10-19 22:00:56 -0400

    improved script

--- bin/generatetags.sh
+++ bin/generatetags.sh
@@ -1,22 +1,43 @@
 #!/bin/sh
 
-cd ../..
-git clone https://github.com/mhwkb/mhwkb.github.io tags_mhwkb_tmp
-cd tags_mhwkb_tmp
+CURRENT_DATE=`date +%Y-%m-%d`
+FILENAME_AND_PATH="tags_tmp.txt"
 
-/bin/echo -e "List of Existing Tags\n" >> tags.txt
-/bin/echo -e "If you need suggestions for tags, this is a good list for reference.\n" >> tags.txt
+/bin/echo -e "List of Existing Tags\n" >> $FILENAME_AND_PATH
+/bin/echo -e "If you need suggestions for tags, this is a good list for reference.\n" >> $FILENAME_AND_PATH
+/bin/echo -e "Last updated $CURRENT_DATE\n" >> $FILENAME_AND_PATH
 
-DATE=`date +%Y-%m-%d`
-/bin/echo -e "Last updated $DATE\n" >> tags.txt
+curl https://github.com/mhwkb/mhwkb.github.io | grep -o '>[^" ]\+.html<' >> $FILENAME_AND_PATH 
 
-ls >> tags.txt
-
-sed -i 's/.html//g' tags.txt
-
-sed -i '/LICENSE/d' tags.txt
-sed -i '/README.md/d' tags.txt
-sed -i '/mhwkb_style.css/d' tags.txt
-sed -i '/tags.txt/d' tags.txt
+sed -i 's/>//g' $FILENAME_AND_PATH
+sed -i 's/<//g' $FILENAME_AND_PATH
+sed -i 's/.html//g' $FILENAME_AND_PATH
 
 exit 0;
+
+# #!/bin/sh
+
+# CURRENT_DATE=`date +%Y-%m-%d`
+# GIT_REPO_WITH_HTML='https://github.com/mhwkb/mhwkb.github.io'
+
+# DEFAULT_TARGET_CLONE_DIR='tags_mhwkb_tmp'
+# TARGET_CLONE_DIR=$DEFAULT_TARGET_CLONE_DIR
+
+# cd ../..
+# git clone $GIT_REPO_WITH_HTML $TARGET_CLONE_DIR
+# cd $TARGET_CLONE_DIR
+
+# /bin/echo -e "List of Existing Tags\n" >> tags.txt
+# /bin/echo -e "If you need suggestions for tags, this is a good list for reference.\n" >> tags.txt
+# /bin/echo -e "Last updated $CURRENT_DATE\n" >> tags.txt
+
+# ls >> tags.txt
+
+# sed -i 's/.html//g' tags.txt
+
+# sed -i '/LICENSE/d' tags.txt
+# sed -i '/README.md/d' tags.txt
+# sed -i '/mhwkb_style.css/d' tags.txt
+# sed -i '/tags.txt/d' tags.txt
+
+# exit 0;
